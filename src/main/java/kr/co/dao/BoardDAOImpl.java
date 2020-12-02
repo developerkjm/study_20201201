0cac7222a19879a06f50f7a95778943f421e114d
developerkjm <jmkang1984@gmail.com>
2020-12-03 02:59:07 +0900

    11.게시판검색기능

--- src/main/java/kr/co/dao/BoardDAOImpl.java
+++ src/main/java/kr/co/dao/BoardDAOImpl.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Repository;
 
 import kr.co.vo.BoardVO;
 import kr.co.vo.Criteria;
+import kr.co.vo.SearchCriteria;
 
 @Repository
 public class BoardDAOImpl implements BoardDAO {
@@ -27,15 +28,15 @@ public class BoardDAOImpl implements BoardDAO {
 
 	// 게시물 목록 조회
 	@Override
-	public List<BoardVO> list(Criteria cri) throws Exception {		
-		return sqlSession.selectList("boardMapper.listPage", cri);
+	public List<BoardVO> list(SearchCriteria scri) throws Exception {		
+		return sqlSession.selectList("boardMapper.listPage", scri);
 	}
 	
 	// 게시물 총 갯수
 	@Override	
-	public int listCount() throws Exception {
+	public int listCount(SearchCriteria scri) throws Exception {
 		// TODO Auto-generated method stub
-		return sqlSession.selectOne("boardMapper.listCount") ; 
+		return sqlSession.selectOne("boardMapper.listCount", scri) ; 
 	}
 	
 	// 게시물 조회
